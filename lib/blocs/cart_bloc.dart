b91af9ffbf583f4869a31828d2bbc4dd5c701764
bingo <>
2019-02-26 23:00:33 +0800

    implemented CartEvent and CartState

--- lib/blocs/cart_bloc.dart
+++ lib/blocs/cart_bloc.dart
@@ -1,52 +1,62 @@
 import 'dart:async';
-import 'package:rxdart/rxdart.dart';
 
-import 'package:shopping_cart/models/cart.dart';
 import 'package:shopping_cart/models/menu_items.dart';
+import 'package:shopping_cart/framework/bloc_base.dart';
 
-class CartBloc {
-  final _cart = Cart();
-
-  final _additionController = StreamController<MenuItem>();
-  Sink<MenuItem> get addition => _additionController.sink;
-
-  final _removalController = StreamController<MenuItem>();
-  Sink<MenuItem> get removal => _removalController.sink;
-
-  final _itemCountSubject = BehaviorSubject<int>(seedValue: 0);
-  Stream<int> get itemCount => _itemCountSubject.stream;
-
-  final _totalPriceSubject = BehaviorSubject<String>(seedValue: '0.00');
-  Stream<String> get totalPrice => _totalPriceSubject.stream;
-
-  final _cartSubject = BehaviorSubject<Cart>(seedValue: Cart());
-  Stream<Cart> get cart => _cartSubject.stream;
-
-  CartBloc() {
-    _additionController.stream.listen(_handleAddition);
-    _removalController.stream.listen(_handleRemoval);
-  }
-
-  void _handleAddition(MenuItem item) {
-    _cart.add(item);
-
-    _itemCountSubject.add(_cart.itemCount);
-    _totalPriceSubject.add(_cart.totalPrice);
-    _cartSubject.add(_cart);
-  }
-
-  void _handleRemoval(MenuItem item) {
-    _cart.remove(item);
-
-    _itemCountSubject.add(_cart.itemCount);
-    _totalPriceSubject.add(_cart.totalPrice);
-    _cartSubject.add(_cart);
-  }
-
-  dispose() {
-    _additionController.close();
-    _removalController.close();
-  }
+class CartEvent extends BlocEvent {
+  final MenuItem item;
+  final int amount;
+  CartEvent({this.item, this.amount});
 }
 
-final cartBloc = CartBloc();
+class CartEventAddition extends CartEvent {
+  CartEventAddition(item, amount) : super(item: item, amount: amount);
+}
+
+class CartEventRemoval extends CartEvent {}
+
+class CartState extends BlocState {
+  Map<MenuItem, int> content;
+
+  CartState({this.content});
+
+  factory CartState.initial() {
+    return CartState(content: {});
+  }
+
+  factory CartState.addedTo(CartState state, MenuItem item, int amount) {
+    Map<MenuItem, int> newContent = Map.from(state.content);
+    newContent.putIfAbsent(item, () => 0);
+    newContent[item] += amount;
+    return CartState(content: newContent);
+  }
+
+  factory CartState.removedFrom(CartState state, MenuItem item, int amount) {
+    assert(state.content.containsKey(item),
+        'Tried to remove a non-exist item from the cart.');
+
+    Map<MenuItem, int> newContent = Map.from(state.content);
+    newContent[item] -= amount;
+
+    assert(
+        newContent[item] >= 0, 'Tried to remove too many items from the cart.');
+
+    return CartState(content: newContent);
+  }
+
+  int get totalItems => content.values.fold(0, (sum, val) => sum + val);
+
+  double get totalPrice => content.entries
+      .fold(0.00, (sum, entry) => sum + entry.key.price * entry.value);
+}
+
+class CartBloc extends BlocBase<CartEvent, CartState> {
+  @override
+  Stream<CartState> eventHandler(CartEvent event, CartState state) async* {
+    if (event is CartEventAddition) {
+      yield CartState.addedTo(state, event.item, event.amount);
+    } else if (event is CartEventRemoval) {
+      yield CartState.removedFrom(state, event.item, event.amount);
+    }
+  }
+}
